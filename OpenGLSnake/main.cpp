8c3780fc3e00b75bf08a0ef76e803818d7f754fe
victo <victo@DESKTOP-5RH1MK1>
2017-12-13 05:31:10 -0200

    Now we have a black screen that can be closed pressing escape

--- OpenGLSnake/main.cpp
+++ OpenGLSnake/main.cpp
@@ -11,6 +11,15 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+//Process Inputs
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
 int main()
 {
 	//Init GLFW
@@ -48,6 +57,7 @@ int main()
 	//Main loop
 	while (!glfwWindowShouldClose(window))
 	{
+		processInput(window);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
