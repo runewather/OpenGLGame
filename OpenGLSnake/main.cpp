21a07331985d6d30477daf5e35985d8ac04f17ba
victo <victo@DESKTOP-5RH1MK1>
2017-12-14 08:43:49 -0200

    Finally we have a f*** big orange triangle

--- OpenGLSnake/main.cpp
+++ OpenGLSnake/main.cpp
@@ -5,6 +5,22 @@
 #define WIDTH 800
 #define HEIGHT 600
 
+//Vertex Shader
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+//Fragment Shader
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
 //Call this function when the window is resized
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -54,6 +70,72 @@ int main()
 	//Set callback function to adjust screen when resized
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	//Triangle Vertices
+	float triangle[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5, -0.5f, 0.0f,
+		 0.0f, 0.5f, 0.0f
+	};
+
+	//Create and set VBO
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	
+	
+	//Create and set VAO
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//Compiling vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	//Check if vertex shader is ok
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR: VERTEX SHADER COMPILATION FAILED\n" << std::endl;
+	}
+
+	//Compiling fragment shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//Creating program object
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	
+	//Setting shaders to program
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	//Check if liking shaders to program is ok
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR: FRAGMENT SHADER COMPILATION FAILED!" << std::endl;
+	}
+
+	//Deleting shaders objects after liking them with the program
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
 	//Main loop
 	while (!glfwWindowShouldClose(window))
 	{
@@ -61,8 +143,14 @@ int main()
 		processInput(window);
 
 		//Redering
+		//Clear Screen
 		glClearColor(0.19f, 0.3f, 0.47f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Set shader program
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//Swap buffers
 		glfwSwapBuffers(window);
