d6cde220d90f638a0eeac757ca07cbae7a67f7df
victo <victo@DESKTOP-5RH1MK1>
2017-12-15 03:04:34 -0200

    First learnopengl.com exercise - Draw 2 triangles next to each other using glDrawArrays

--- OpenGLSnake/main.cpp
+++ OpenGLSnake/main.cpp
@@ -117,11 +117,13 @@ int main()
 	//Set up vertex data
 
 	//Rectangle Vertices
-	float rectangle[] = {
-		0.5f,  0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f, // bottom left
-		-0.5f,  0.5f, 0.0f	// top left
+	float triangles[] = {
+		-1.0f, -0.5f, 0.0f,
+		0.0f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f,
+		0.0f, -0.5f, 0.0f,
+		1.0f, -0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f
 	};
 
 	//Rectangle Indicies
@@ -138,11 +140,11 @@ int main()
 
 	//Send data to VBO buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle), rectangle, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangles), triangles, GL_STATIC_DRAW);
 
 	//Send data to EBO buffer
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
 
 	//Set the data format and location
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -166,7 +168,8 @@ int main()
 		//Set shader program
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		//Swap buffers
 		glfwSwapBuffers(window);
